9a296cbb85a6636e0ffb6b0d36c749d9769e4c91
Sera Yang <sera.yang@berkeley.edu>
2017-10-01 15:58:06 -0700

    push

--- test_github.py
+++ test_github.py
@@ -3,4 +3,6 @@ import subprocess
 # content of test_sample.py
 def test_attemp():
     process = subprocess.Popen(["git", "hub", "sync"])
-    process = subprocess.Popen(["cd", ".git/git_hub"])
+    process = subprocess.Popen(["cd", ".git/git-hub"])
+    process = subprocess.Popen(["git", "hub", "search", "-c", "test"], stdout=subprocess.PIPE)
+    assert "help" == str(process.stdout.read())
