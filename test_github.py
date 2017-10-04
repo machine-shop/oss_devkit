a37873abddf805e9029b19dc0f34f40e71808616
Sera Yang <sera.yang@berkeley.edu>
2017-10-04 14:57:15 -0700

    still not sure how to use travis

--- test_github.py
+++ test_github.py
@@ -3,5 +3,11 @@ import subprocess
 # content of test_sample.py
 def test_attemp():
     process = subprocess.Popen(["git", "hub", "sync"])
-    process = subprocess.Popen(["ls"], stdout=subporcess.PIPE)
-    assert "WHAT" == str(process.stdout.read())
+    process = subprocess.Popen(["ls"], stdout=subprocess.PIPE)
+    assert "OutsideHacks" in str(process.stdout.read())
+    process = subprocess.Popen(["cd", "OutsideHacks/.git/git-hub"])
+    process = subprocess.Popen(["git", "hub", "sync"])
+    process = subprocess.Popen(["ls"],stdout=subprocess.PIPE)
+    assert "pull-requests.toml" in str(process.stdout.read())
+    process = subprocess.Popen(["git", "hub", "search", "empty"], stdout=subprocess.PIPE)
+    assert str(process.stdout.read()) == '5 \x1b[0;32;40m 0 \x1b[0m Sera Yang/another-pull empty : 2017-09-18'
