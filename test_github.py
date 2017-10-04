25156db70e2781924d0db716e2fb042fcedd4ac5
Sera Yang <sera.yang@berkeley.edu>
2017-10-04 08:51:54 -0700

    testing travis cloning

--- test_github.py
+++ test_github.py
@@ -3,6 +3,5 @@ import subprocess
 # content of test_sample.py
 def test_attemp():
     process = subprocess.Popen(["git", "hub", "sync"])
-    process = subprocess.Popen(["cd", "OutsideHacks/.git/git-hub"])
-    process = subprocess.Popen(["git", "hub", "search", "-c", "test"], stdout=subprocess.PIPE)
-    assert "help" == str(process.stdout.read())
+    process = subprocess.Popen(["ls"], stdout=subporcess.PIPE)
+    assert "WHAT" == str(process.stdout.read())
