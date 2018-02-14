1112e3065196dc1a33f0490eb34dd075008ff51d
Sera Yang <sera.yang@berkeley.edu>
2018-02-14 13:33:37 -0800

    cleaning up syntax

--- webpage.py
+++ webpage.py
@@ -6,15 +6,16 @@ from datetime import datetime
 from datetime import timedelta
 from os.path import join as pjoin
 
+
 def path_to_git():
     """Finds path to .git folder
     """
     path_repo = os.path.abspath('.')
-    # if not in directory with .git, keep going back to find file
     while os.path.abspath(path_repo) != '/' and not os.path.isdir(pjoin(path_repo, '.git')):
         path_repo = pjoin(path_repo, '..')
     path_git = pjoin(path_repo, ".git")
     return path_git
+
 
 def path_to_toml():
     """Finds path to pull-requests.toml
@@ -24,19 +25,20 @@ def path_to_toml():
     path_prs = pjoin(path_github, 'pull-requests.toml')
     return path_prs
 
+
 def week_old_comments():
     """Main function to find PRs that haven't been commented on in over a week
     """
     try:
         path_prs = path_to_toml()
         f = open(path_prs, "r")
-        pr_dict = toml.load(f)      # fetches toml file and creates a dictionary
+        pr_dict = toml.load(f)
     except (OSError, IOError) as e:
-        # if pull-requests.toml hasnt been created yet calls sync and then reties to fetch
         print("ERROR: pull-requests.toml doesn't exist. Run 'git hub sync' and try again")
     open_dict = pr_dict['open pull requests']
     opened = week_old_comments_helper(open_dict)
     return opened
+
 
 def week_old_comments_helper(prs):
     """Helper function that finds all PRs in a dictionary that are over a week old
@@ -45,14 +47,13 @@ def week_old_comments_helper(prs):
     for num in list(prs.keys()):
         pr = prs[num]
         if(pr['most_recent_comment'] == ""):
-            #all_prs.append('PR #{0}: {1}/{2} {3}'.format(sub_keys, sub_dictionary['user'], sub_dictionary['branch'], sub_dictionary['comment']))
             all_prs.append(f'PR #{num}: {pr["user"]}/{pr["branch"]}: {pr["comment"]}')
         else:
             comment_time = parse_time(pr['most_recent_comment'])
             if((datetime.now() - comment_time).days > 7):
-                #all_prs.append('PR #{0}: {1}/{2} {3}'.format(sub_keys, sub_dictionary['user'], sub_dictionary['branch'], sub_dictionary['comment']))
                 all_prs.append(f'PR #{num}: {pr["user"]}/{pr["branch"]}: {pr["comment"]}')
     return all_prs
+
 
 def parse_time(time):
     """
@@ -68,24 +69,25 @@ def parse_time(time):
     year, date = date.split('-', 1)
     month, date = date.split('-', 1)
     day = date
-    hour, time = time.split(':',1)
-    minute, time = time.split(':',1)
+    hour, time = time.split(':', 1)
+    minute, time = time.split(':', 1)
     second = time
     d = datetime(int(year), int(month), int(day), int(hour), int(minute), int(second))
     return d
+
 
 def oldest_prs():
     """Finds oldest prs by date created"""
     try:
         path_prs = path_to_toml()
         f = open(path_prs, "r")
-        pr_dict = toml.load(f)      # fetches toml file and creates a dictionary
+        pr_dict = toml.load(f)
     except (OSError, IOError) as e:
-        # if pull-requests.toml hasnt been created yet calls sync and then reties to fetch
         print("ERROR: pull-requests.toml doesn't exist. Run 'git hub sync' and try again")
     open_dict = pr_dict['open pull requests']
     oldest = oldest_prs_helper(open_dict)
     return oldest
+
 
 def oldest_prs_helper(prs):
     """Helper function that finds oldest prs"""
@@ -109,24 +111,25 @@ def oldest_prs_helper(prs):
             oldest.append(f'PR #{pr_num}: {pr["user"]}/{pr["branch"]}: {pr["title"]} --  {days}')
     return oldest
 
+
 def most_active_prs():
     """Finds most active prs by most comments"""
     try:
         path_prs = path_to_toml()
         f = open(path_prs, "r")
-        pr_dict = toml.load(f)      # fetches toml file and creates a dictionary
+        pr_dict = toml.load(f)
     except (OSError, IOError) as e:
-        # if pull-requests.toml hasnt been created yet calls sync and then reties to fetch
         print("ERROR: pull-requests.toml doesn't exist. Run 'git hub sync' and try again")
     open_dict = pr_dict['open pull requests']
     most_popular = most_active_prs_helper(open_dict)
     return most_popular
 
+
 def most_active_prs_helper(prs):
     """helper function to find pull requests."""
     popular = []
     q = PriorityQueue()
-    for pr_num in list(prs.keys()): #find in last two weeks, not all time
+    for pr_num in list(prs.keys()):
         pr = prs[pr_num]
         recent_comment_count = recent_comments(pr)
         q.put(-recent_comment_count)
@@ -136,7 +139,7 @@ def most_active_prs_helper(prs):
     while q.qsize() != 0 and count != 3:
         most_comments.add(-q.get())
         count += 1
-    most_comments.discard(0) #removes 0 because don't want to have pr with 0 comments
+    most_comments.discard(0)
     for pr_num in list(prs.keys()):
         pr = prs[pr_num]
         recent_comment_count = recent_comments(pr)
@@ -144,8 +147,9 @@ def most_active_prs_helper(prs):
             popular.append(f'PR #{pr_num}: {pr["user"]}/{pr["branch"]}: {pr["title"]} -- {recent_comment_count} comment(s)')
     return popular
 
+
 def recent_comments(pr):
-    #counts how many comments in the last two weeks
+    """Counts how many comments in the last two weeks"""
     count = 0
     comment_dates = pr['comment_dates']
     for date in comment_dates:
@@ -155,19 +159,18 @@ def recent_comments(pr):
     return count
 
 
-
 def no_discussion():
     """Finds PRs that haven't seen any discussion. """
     try:
         path_prs = path_to_toml()
         f = open(path_prs, "r")
-        pr_dict = toml.load(f)      # fetches toml file and creates a dictionary
+        pr_dict = toml.load(f)
     except (OSError, IOError) as e:
-        # if pull-requests.toml hasnt been created yet calls sync and then reties to fetch
         print("ERROR: pull-requests.toml doesn't exist. Run 'git hub sync' and try again")
     open_dict = pr_dict['open pull requests']
     opened = no_discussion_helper(open_dict)
     return opened
+
 
 def no_discussion_helper(prs):
     """helper function to find pull requests."""
@@ -178,18 +181,19 @@ def no_discussion_helper(prs):
             no_disc_prs.append(f'PR #{pr_num}: {pr["user"]}/{pr["branch"]}: {pr["title"]}')
     return no_disc_prs
 
+
 def prs_with_me():
     """Finds PRs that I have commented on"""
     try:
         path_prs = path_to_toml()
         f = open(path_prs, "r")
-        pr_dict = toml.load(f)      # fetches toml file and creates a dictionary
+        pr_dict = toml.load(f)
     except (OSError, IOError) as e:
-        # if pull-requests.toml hasnt been created yet calls sync and then reties to fetch
         print("ERROR: pull-requests.toml doesn't exist. Run 'git hub sync' and try again")
     open_dict = pr_dict['open pull requests']
     opened = find_prs_with_me(open_dict)
     return opened
+
 
 def find_prs_with_me(prs):
     """helper function to find pull requests."""
@@ -206,13 +210,13 @@ def unmergeable_prs():
     try:
         path_prs = path_to_toml()
         f = open(path_prs, "r")
-        pr_dict = toml.load(f)      # fetches toml file and creates a dictionary
+        pr_dict = toml.load(f)
     except (OSError, IOError) as e:
-        # if pull-requests.toml hasnt been created yet calls sync and then reties to fetch
         print("ERROR: pull-requests.toml doesn't exist. Run 'git hub sync' and try again")
     open_dict = pr_dict['open pull requests']
     opened = find_unmergeable_prs(open_dict)
     return opened
+
 
 def find_unmergeable_prs(prs):
     """helper function to find pull requests."""
@@ -223,18 +227,19 @@ def find_unmergeable_prs(prs):
             unmergeable_prs.append(f'PR #{pr_num}: {pr["user"]}/{pr["branch"]}: {pr["title"]}')
     return unmergeable_prs
 
+
 def issues_no_comments():
     """Finds issues without any comments. """
     try:
         path_prs = path_to_toml()
         f = open(path_prs, "r")
-        pr_dict = toml.load(f)      # fetches toml file and creates a dictionary
+        pr_dict = toml.load(f)
     except (OSError, IOError) as e:
-        # if pull-requests.toml hasnt been created yet calls sync and then reties to fetch
         print("ERROR: pull-requests.toml doesn't exist. Run 'git hub sync' and try again")
     open_dict = pr_dict['issues']
     opened = find_issues_no_comments(open_dict)
     return opened
+
 
 def find_issues_no_comments(issues):
     """helper function to find issues with no comments."""
@@ -246,8 +251,9 @@ def find_issues_no_comments(issues):
             issues_no_comments.append(f'issue #{num}: {issue["title"]}')
     return issues_no_comments
 
+
 def tickets_referred(comments):
-    #finds tickets in comments of pull requests or issues
+    """Finds tickets in comments of pull requests or issues"""
     issues = []
     for comment in comments:
         if "#" in comment:
@@ -257,14 +263,15 @@ def tickets_referred(comments):
                 issues.append(ticket)
     return issues
 
+
 def closed_pr_refer_ticket():
     """Finds issues that are referred to from prs and other issues """
     try:
         path_prs = path_to_toml()
         f = open(path_prs, "r")
-        pr_dict = toml.load(f)    # fetches toml file and creates a dictionary
+        pr_dict = toml.load(f)
     except (OSError, IOError) as e:
-        # if pull-requests.toml hasnt been created yet calls sync and then reties to fetch
+
         print("ERROR: pull-requests.toml doesn't exist. Run 'git hub sync' and try again")
     closed = pr_dict['closed pull requests']
     issues = pr_dict['issues']
@@ -283,19 +290,21 @@ def find_closed_pr_refer_ticket(closed_prs, issues):
                 unresolved_issues.append(f'issue #{ticket}: {issues[ticket]["title"]}')
     return unresolved_issues
 
+
 def popular_tickets():
     """tickets that are referred to many times"""
     try:
         path_prs = path_to_toml()
         f = open(path_prs, "r")
-        pr_dict = toml.load(f)    # fetches toml file and creates a dictionary
+        pr_dict = toml.load(f)
     except (OSError, IOError) as e:
-        # if pull-requests.toml hasnt been created yet calls sync and then reties to fetch
+
         print("ERROR: pull-requests.toml doesn't exist. Run 'git hub sync' and try again")
     opened = pr_dict['open pull requests']
     issues = pr_dict['issues']
     popular = find_popular_tickets(opened, issues)
     return popular
+
 
 def find_popular_tickets(opened, issues):
     """helper function to find popular tickets"""
@@ -325,7 +334,6 @@ def find_popular_tickets(opened, issues):
         popular_count.add(-q.get())
         count += 1
     most_popular = []
-    #most_popular.remove(1) #is it considered popular if there is only one comment
     for ticket in list(tickets.keys()):
         if tickets[ticket] in popular_count:
             most_popular.append(f'issue #{ticket}: {issues[ticket]["title"]}')
