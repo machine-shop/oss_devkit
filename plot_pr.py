ce25f1c6effce209808b3843869459629cd3e284
Sera Yang <sera.yang@berkeley.edu>
2018-04-10 01:18:14 -0700

    labels are correct, but when the PRs are cloned seems to be slightly off

--- plot_pr.py
+++ plot_pr.py
@@ -40,6 +40,15 @@ def execute():
 
 
     def fetch_PRs(user, repo, state='open'):
+        """Pulls down all the information
+
+        Parameters
+        ----------
+        user : string
+            user's repo we are pulling from
+        repo : string
+            name of repo we are pulling from
+        """
         params = {'state': state,
                   'per_page': 100,
                   'page': 1}
@@ -74,30 +83,57 @@ def execute():
 
 
     def seconds_from_epoch(dates):
+        """Difference in times between dates given and the first PR.
+
+        Parameters
+        ----------
+        dates : list
+            list of times to compare against the first PR
+        """
         seconds = [(dt - epoch).total_seconds() for dt in dates]
         return seconds
 
 
     def get_month_bins(dates):
+        """Counts month_duration number of months back from current time.
+
+        Parameters
+        ----------
+        dates : list
+            list of times to compare against the first PR
+        """
         now = datetime.now(tz=dates[0].tzinfo)
         this_month = datetime(year=now.year, month=now.month, day=1,
                               tzinfo=dates[0].tzinfo)
-        print(month_duration)
-        print("relativedelta: ", [relativedelta(months=i) for i in reversed(range(-1, month_duration))])
         bins = [this_month - relativedelta(months=i)
                 for i in reversed(range(-1, month_duration+3))]
-        print("BINS2: ",bins)
-        # print("FORMATED BINS: ", [date_formatter(val, None) for val in bins])
         return seconds_from_epoch(bins)
 
 
     def diff_month(d1, d2):
+        """Finds the difference in months between two datetime objects
+
+        Parameters
+        ----------
+        d1 : Datetime
+            Second datetime object we are comparing
+        d2 : Datetime
+            Second datetime object we are comparing
+        """
         return (d1.year - d2.year) * 12 + d1.month - d2.month
 
     def date_formatter(value, _):
+        """Formats from seconds from epoch to YYYY/MM.
+
+        Parameters
+        ----------
+        value : int
+            Number of seconds from epoch
+        """
         dt = epoch + timedelta(seconds=value)
         return dt.strftime('%Y/%m')
 
+    # Try to either pull down or fetch from cache all the PRs
     try:
         PRs = json.loads(open(cache, 'r').read())
         print('Loaded PRs from cache...')
@@ -106,97 +142,50 @@ def execute():
         user, repo = login()
         PRs = fetch_PRs(user=user, repo=repo, state='closed')
         PRs.extend(fetch_PRs(user=user, repo=repo, state='open'))
-        PRs.extend(fetch_PRs(user=user, repo=repo, state='closed'))
 
         cf = open(cache, 'w')
         cf.write(json.dumps(PRs))
         cf.flush()
-    # Obtain release dates using, e.g.,
-    #
-    #  git log v0.4 -n 1 --format='%ai'
-    #
-    # The first two releases are commented out.
-    # This was in the era before PRs.
-    #
+    nrs = [pr['number'] for pr in PRs]
+    print('Processing %d pull requests...' % len(nrs))
+    # All the dates a PR was created
+    dates = [dateutil.parser.parse(pr['created_at']) for pr in PRs]
+    print([pr['title'] for pr in PRs])
+    print(dates)
+
+    # Finds all the release dates (tags)
     p = subprocess.run(["git", "log", "--tags", "--simplify-by-decoration", "--pretty='format:%ai %d'"], stdout=subprocess.PIPE)
     tag_dates = str(p.stdout.decode("utf-8").strip('\n'))
-
     releases = OrderedDict([])
-    if len(tag_dates) == 0:
-        dates = [dateutil.parser.parse(pr['created_at']) for pr in PRs]
-        if len(dates) == 0:
-            print("no pull requests!")
-            return
-        first_pr = str(dates[dates.index(min(dates))])
-        releases.update({'0.0': first_pr})
-    else:
-        for release in tag_dates:
-            if not 'tag' in release:
-                continue
-            date = release[:26]
-            tag = release.split('tag: ')[1]
-            tag = tag[:len(tag) - 2]
-            releases.update({tag: date})
-    month_duration = abs(diff_month(dates[0], datetime.now(tz=dates[0].tzinfo)))
-
-
-
+    for release in tag_dates:
+        if not 'tag' in release:
+            continue
+        date = release[:26]
+        tag = release.split('tag: ')[1]
+        tag = tag[:len(tag) - 2]
+        releases.update({tag: date})
+    # Formats all the release dates from strings to Datetime objects
     for r in releases:
         releases[r] = dateutil.parser.parse(releases[r])
 
-    print(releases)
+    # How many months we want to plot, difference between first PR and now
+    month_duration = abs(diff_month(dates[0], datetime.now(tz=dates[0].tzinfo)))
 
-    try:
-        PRs = json.loads(open(cache, 'r').read())
-        print('Loaded PRs from cache...')
-
-    except IOError:
-        user, repo = login()
-        PRs = fetch_PRs(user=user, repo=repo, state='closed')
-        PRs.extend(fetch_PRs(user=user, repo=repo, state='open'))
-        PRs.extend(fetch_PRs(user=user, repo=repo, state='closed'))
-
-        cf = open(cache, 'w')
-        cf.write(json.dumps(PRs))
-        cf.flush()
-
-    nrs = [pr['number'] for pr in PRs]
-    print('Processing %d pull requests...' % len(nrs))
-
-    # dates = [dateutil.parser.parse(pr['created_at']) for pr in PRs]
-    #
-    # if len(dates) == 0:
-    #     print("no pull requests!")
-    #     return
-    #
-    # first_pr = dates[dates.index(min(dates))]   #time of first pr
-    first_pr = dates[0]
-    start_date = first_pr#datetime(2016, 5, 1, tzinfo=dates[0].tzinfo)   #Not sure why, but seems like graph tick marks start at 5/1/2016
-
-    # print("FIRST PR: ", first_pr)
-    # start_month = diff_month(first_pr, start_date)-1    #difference between first pr and 5/1/2016
-
-    # Fixed point in time
-    epoch = first_pr#datetime(209, 1, 1, tzinfo=dates[0].tzinfo)
-    print("EPOCH: ", epoch)
-    print("DATES: ", dates)
+    # First PR
+    epoch = min(dates)
+    # The time between each PR and the first PR
     dates_f = seconds_from_epoch(dates)
-    print("DATES_F: ", dates_f)
     bins = get_month_bins(dates)
     bins.sort()
-    print("BINS: ",bins)
     fig, ax = plt.subplots(figsize=(7, 5))
     n, bins, _ = ax.hist(dates_f, bins=bins, color='blue', alpha=0.6)
 
-    tick_space = math.ceil(diff_month(datetime.now(tz=dates[0].tzinfo), first_pr)/10)
+    tick_space = math.ceil(diff_month(datetime.now(tz=dates[0].tzinfo), epoch)/10)
 
     ax.xaxis.set_major_formatter(FuncFormatter(date_formatter))
-    # print("start month: ",start_month)
-    # bins = bins - 5.0e+5
-    print(bins[0:-1:tick_space])
-    print("START: ", date_formatter(bins[0], None), " END: ", date_formatter(bins[-1], None))
-    ax.set_xticks(bins[0:-1:tick_space])  # Date label every 3 months. //TODO - set first number to correct start month
+    ax.set_xticks(bins[0:-1:tick_space])
     ax.set_xlim(bins[0], bins[-1])
+
     labels = ax.get_xticklabels()
     for l in labels:
         l.set_rotation(40)
@@ -223,5 +212,3 @@ def execute():
 
     plt.tight_layout()
     fig.savefig('.git/git-hub/PRs.png')
-
-    # plt.show()
